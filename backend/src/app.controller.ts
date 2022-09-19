cef315a94af2e7fcdfdb0a6908c771154768f127
DrTr4p <65681890+kasdo770@users.noreply.github.com>
2022-09-20 01:53:47 +0200

    revenue

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,32 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Redirect, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Prisma, PrismaClient } from '@prisma/client'
+import { Request } from '@nestjs/common';
 const prisma = new PrismaClient()
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  async getHello() {
+  
+  @Post()
+  async getHello( @Req() request: Request ) {
     const ApBill = await prisma.projectBill.create({
       data :{
-        name: '30د',
-        revenues: {
-          //@ts-ignore
-          createMany:{
-            data:[
-              {
-                amount:20,
-                date:new Date('2020')
+        //@ts-ignore
+        name: request.body.name,
+        //@ts-ignore
+        precentage : request.body.precentage
+    }  
+  })
+      //@ts-ignore
+      const revenues = request.body.revenues
+      console.log(revenues.length)
+      for(let i = 0 ; i < revenues.length; i++){
+        console.log(revenues[i])
+        const rev = await prisma.revenue.create({
+          data: {
+            //@ts-ignore
+            amount: request.body.revenues[i].amount,
+            //@ts-ignore
+            date: new Date(request.body.revenues[i].date),
+            ProjectBill:{
+              connect:{
+                id : ApBill.id
               }
-            ]
-          }
-        },
+            }
+          } 
+         
+        })
+        console.log(rev)
       }
-    })
-
-
-    return ApBill
+    
+  
   }
-}
+
+  }
+
+
+
