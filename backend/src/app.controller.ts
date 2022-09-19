49b120d2a31ce652d674cf5291bdffc0c693d530
DrTr4p <65681890+kasdo770@users.noreply.github.com>
2022-09-19 19:40:55 +0200

    Schema second try

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    const ApBill = await prisma.anotherPaymentsBill.create({
+      //@ts-ignore
+      data: {
+        Amount: 25,
+        Date : '2022',
+        InReturn : "Something"
+      }
+    })
+
+    console.log(ApBill)
   }
 }
