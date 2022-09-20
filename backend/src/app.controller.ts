974a204dc90ae0eda7cf90939c7b685cb629bc71
DrTr4p <65681890+kasdo770@users.noreply.github.com>
2022-09-20 21:44:36 +0200

    createBill without workers

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,49 +1,17 @@
 import { Controller, Get, Post, Redirect, Req } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Prisma, PrismaClient } from '@prisma/client'
 import { Request } from '@nestjs/common';
-const prisma = new PrismaClient()
+
+
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  
+
   @Post()
-  async getHello( @Req() request: Request ) {
-    const ApBill = await prisma.projectBill.create({
-      data :{
-        //@ts-ignore
-        name: request.body.name,
-        //@ts-ignore
-        precentage : request.body.precentage
-    }  
-  })
-      //@ts-ignore
-      const revenues = request.body.revenues
-      console.log(revenues.length)
-      for(let i = 0 ; i < revenues.length; i++){
-        console.log(revenues[i])
-        const rev = await prisma.revenue.create({
-          data: {
-            //@ts-ignore
-            amount: request.body.revenues[i].amount,
-            //@ts-ignore
-            date: new Date(request.body.revenues[i].date),
-            ProjectBill:{
-              connect:{
-                id : ApBill.id
-              }
-            }
-          } 
-         
-        })
-        console.log(rev)
-      }
-    
-  
+  async CreateBill(@Req() request: Request) {
+    console.log("phase1")
+    return await this.appService.createNewBill(request)
   }
-
-  }
-
-
-
+}
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,83 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { Controller, Get, Post, Redirect, Req , HttpException, HttpStatus} from '@nestjs/common';
+import { Request } from '@nestjs/common';
+const prisma = new PrismaClient();
+
+async function createBill(reqBody) {
+  console.log("phase3")
+  console.log(reqBody.expenses)
+  if (reqBody.expenses && reqBody.revenues) {
+    const projectBill = await prisma.projectBill.create({
+      data: {
+        name: reqBody.name,
+        officePrecentage: reqBody.precentage,
+      },
+    });
+
+    const expenses = reqBody.expenses;
+    for (let i = 0; i < expenses.length; i++) {
+      const rev = await prisma.expenses.create({
+        data: {
+          totalcost: expenses[i].totalcost,
+          date: new Date(expenses[i].date),
+          materialName: expenses[i].materialName,
+          day: expenses[i].day,
+          billCode: expenses[i].billCode,
+          ProjectBill: {
+            connect: {
+              id: projectBill.id,
+            },
+          },
+        },
+      });
+    }
+    const revenues = reqBody.revenues;
+    for (let i = 0; i < revenues.length; i++) {
+      console.log(revenues[i]);
+      const rev = await prisma.revenue.create({
+        data: {
+          amount: revenues[i].amount,
+          date: new Date(revenues[i].date),
+          ProjectBill: {
+            connect: {
+              id: projectBill.id,
+            },
+          },
+        },
+      });
+    }
+  } else {
+    console.log('Error')
+    throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+  }
+}
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async createNewBill(request: Request) {
+    console.log('phase2')
+    let reqBody  = request.body as any
+    let oldBill  = await prisma.projectBill.findFirst({
+      where: {
+        name: reqBody.name,
+      },
+    }) as any;
+    
+    
+    try {
+      if (oldBill.expenses && oldBill.revenues) {
+        createBill(reqBody);
+      } else {
+        await prisma.projectBill.delete({
+          where: {
+            name: oldBill.name,
+          },
+        });
+      }
+    } catch (e) {
+      createBill(reqBody);
+    }
   }
 }
