2d0b1409e8c7485d9d499862211b7420b80ac044
Sekai <50212311+ig4e@users.noreply.github.com>
2022-09-27 19:14:45 +0200

    -

--- app/renderer/components/Modal.tsx
+++ app/renderer/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { XCircleIcon } from "@heroicons/react/24/outline";
 import { DocumentMagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { DocumentIcon } from "@heroicons/react/24/outline";
@@ -6,51 +6,62 @@ import { DocumentIcon } from "@heroicons/react/24/outline";
 import NormalBill from "../public/images/logo.png";
 
 import Image from "next/image";
+import Link from "next/link";
+
+function useOutsideAlerter(ref: any, callback: any) {
+	useEffect(() => {
+		/**
+		 * Alert if clicked on outside of element
+		 */
+		function handleClickOutside(event) {
+			if (ref.current && !ref.current.contains(event.target)) {
+				callback("You clicked outside of me!");
+			}
+		}
+		// Bind the event listener
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => {
+			// Unbind the event listener on clean up
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, [ref]);
+}
 
 function Modal({ setOpenModal }) {
+	const modalRef = useRef();
+	useOutsideAlerter(modalRef, () => setOpenModal(false));
 	return (
-		<div className="fixed z-50 inset-0 bg-black/50 grid place-items-center">
-			<div className="bg-base max-w-2xl w-full p-6 rounded">
-				<div className=" flex justify-between">
+		<div className="fixed z-50 inset-0 bg-primary/25 grid place-items-center">
+			<div className="bg-base p-6 rounded" ref={modalRef}>
+				<div className="flex justify-between items-center">
 					<h1 className="font-bold text-2xl"> اضافة فاتورة جديدة</h1>
 					<XCircleIcon
 						onClick={() => {
 							setOpenModal(false);
 						}}
-						className="h-61 w-6 cursor-pointer"
+						className="h-8 w-8 cursor-pointer"
 					></XCircleIcon>
 				</div>
 				<div className="my-1">
 					<h1>اختار نوع الفاتورة</h1>
 				</div>
-				<div className="flex justify-between mx-16 mt-8 mb-8">
-					<div>
-                        <button className="focus:outline-none hover:outline-dashed focus:ring focus:ring-primary">
-                        <DocumentMagnifyingGlassIcon className="w-40 h-40"></DocumentMagnifyingGlassIcon>
-				
-                        </button>
-                   		<h1 className="mr-10">فاتورة عامة</h1>
-					</div>
-
-					<div>
-						<button className="focus:outline-none hover:outline-dashed focus:ring focus:ring-primary">
-                        <DocumentIcon className="w-40 h-40"></DocumentIcon>
-                        </button>
-						<h1 className="mr-10">فاتورة اخرى</h1>
-					</div>
-				</div>
-				<div className="flex justify-start mt-4">
-					<button className=" mx-4 bg-primary drop-shadow-lg text-white text-xl font-semibold flex items-center gap-2 px-2 py-1 rounded-md hover:bg-primary/80 active:bg-primary transition">
-						اكمل
-					</button>
-					<button
-						className=" mx-4 bg-white drop-shadow-lg text-primary text-xl font-semibold flex items-center gap-2 px-2 py-1 rounded-md hover:bg-white/10 active:bg-white/5 transition"
-						onClick={() => {
-							setOpenModal(false);
-						}}
-					>
-						الغاء
-					</button>
+				<div className="flex justify-between mt-8 mb-8 gap-12">
+					<Link href="/create">
+						<a>
+							<button className="focus:outline-none outline outline-1 rounded-md focus:ring focus:ring-primary">
+								<DocumentMagnifyingGlassIcon className="w-40 h-40"></DocumentMagnifyingGlassIcon>
+							</button>
+							<h1 className="text-center">فاتورة عامة</h1>
+						</a>
+					</Link>
+					<Link href="/create-another-bill">
+						<a>
+							<button className="focus:outline-none outline outline-1 rounded-md focus:ring focus:ring-primary">
+								<DocumentIcon className="w-40 h-40"></DocumentIcon>
+							</button>
+							<h1 className="text-center">فاتورة اخرى</h1>
+						</a>
+					</Link>
 				</div>
 			</div>
 		</div>
