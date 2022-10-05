b7b7d40e3ee3f998e8564a7321580865f7bddf82
Sekai <50212311+ig4e@users.noreply.github.com>
2022-10-05 19:54:09 +0200

    login done

--- frontend/renderer/pages/login.tsx
+++ frontend/renderer/pages/login.tsx
@@ -6,20 +6,24 @@ import Logo from "../public/images/logo.png";
 function Login() {
 	return (
 		<div className="absolute inset-x-0 w-full top-14 bottom-0 grid place-items-center">
-			<div className="max-w-sm rounded-md border-2 py-16 w-full flex flex-col items-center p-4">
-				<div className="w-36 h-36 rounded">
-					<Image src={Logo}></Image>
+			<div className="drop-shadow-lg bg-base max-w-xs rounded-md border-2 gap-4 pb-16 w-full flex flex-col items-center p-4">
+				<div className="flex flex-col items-center mb-4 ">
+					<div className="w-36 h-36 ">
+						<Image src={Logo} className=""></Image>
+					</div>
+					<h1 className="font-semibold text-xl mt-2">
+						S.H Steel Construction
+					</h1>
 				</div>
-				<h1 className="font-semibold text-xl">
-					S.H Steel Construction
-				</h1>
 
 				<Input
 					type={"text"}
 					lclassName="text-center"
-					iclassName="placeholder:text-center"
+					iclassName="placeholder:text-center text-center h-12"
 					placeholder={"Password"}
 				></Input>
+
+				<button className="btn-primary py-1">Login</button>
 			</div>
 		</div>
 	);
