0705cf962decf9126af1460f7f7d4e41d305bbf7
DrTr4p <65681890+kasdo770@users.noreply.github.com>
2022-10-28 13:28:13 +0200

    aa

--- frontend/renderer/pages/index.tsx
+++ frontend/renderer/pages/index.tsx
@@ -13,84 +13,15 @@ import BillTypeModal from "../components/BillTypeModal";
 import Table from "../components/Table";
 import {  AnotherPaymentsBill, ProjectBill, useStore } from "../store";
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-import * as status from 'http-status'
-import * as createError from 'http-errors'
+
+import {filter} from './main'
 
 interface IProps {
 	publicBills: ProjectBill[];
 	officeBills: AnotherPaymentsBill[];
 }
 
-async function filter(
-    pageReq: number,
-    limitReq: number,
-    bpageReq: number,
-    blimitReq: number,
-  ) {
-    console.log(pageReq , limitReq , bpageReq , blimitReq)
-    let anotherBills = { pagination: {}, data: {} } as any;
-    let projectbill = { pagination: {}, data: {} } as any;
-    let result = {};
-    console.log(pageReq , limitReq , bpageReq , blimitReq)
-    const [aBCount, bCount] = await Promise.all([
-      await prisma.anotherPaymentsBill.count(),
-      await prisma.projectBill.count(),
-    ]);
-    const abIndex = pageReq * limitReq;
-    const bindex = bpageReq * blimitReq;
-    const bskipindex = (bpageReq - 1) * blimitReq;
-    const abskipindex = (pageReq - 1) * limitReq;
-    const totalabPages = Math.round(aBCount / Number(limitReq));
-    const totalBPages = Math.round(bCount / Number(blimitReq));
-  
-    anotherBills.pagination.currentPage = Number(pageReq);
-    anotherBills.pagination.pageSize = Number(limitReq);
-    anotherBills.pagination.totalCount = aBCount;
-    anotherBills.pagination.totalPages = totalabPages;
-  
-    projectbill.pagination.currentPage = Number(bpageReq);
-    projectbill.pagination.pageSize = Number(blimitReq);
-    projectbill.pagination.totalCount = bCount;
-    projectbill.pagination.totalPages = totalBPages;
-  
-    if (abIndex < aBCount) {
-      anotherBills.pagination.lastPage = false;
-    } else {
-      anotherBills.pagination.lastPage = true;
-    }
-  
-    if (bindex < bCount) {
-      projectbill.pagination.lastPage = false;
-    } else {
-      projectbill.pagination.lastPage = true;
-    }
-  
-    try {
-      projectbill.data = await prisma.projectBill.findMany({
-        take: Number(blimitReq),
-        skip: Number(bskipindex),
-      });
-    } catch (e) {
-      console.log(e);
-      throw new Error(e);
-    }
-  
-    try {
-      anotherBills.data = await prisma.anotherPaymentsBill.findMany({
-        take: Number(limitReq),
-        skip: Number(abskipindex),
-      });
-    } catch (e) {
-      console.log(e);
-      throw new createError(status.FORBIDDEN , e)
-    }
-  
-    return JSON.parse(JSON.stringify({result : {
-		projectBills: projectbill,
-		anotherBills: anotherBills,
-	  }}));
-  }
+
 
 
 //@ts-ignore
@@ -135,13 +66,12 @@ const IndexPage = ({ publicBills, officeBills, PAGE }: IProps) => {
 		}
 	}
 		async function handlePagination(lalal , paga ){
+		const prisma = new PrismaClient()
 		console.log(lalal)
 		console.log('paga'  +  paga)
 	
-		const { data: billsData } = await axios({
-			url: `http://localhost:3000/getAll?abpage=${paga}&ablimit=${pagesize}&bpage=${lalal}&blimit=${pagesize}`,
-		});	
-		const { projectBills, anotherBills } = billsData;
+		const  { result : data} = await filter(paga , pagesize , paga , pagesize, prisma)
+		const { projectBills, anotherBills } = data;
 		setHomePublicBills(projectBills.data);
 		setHomeOfficeBills(anotherBills.data);
 		return {
@@ -262,12 +192,12 @@ const IndexPage = ({ publicBills, officeBills, PAGE }: IProps) => {
 export default IndexPage;
 
 export const getServerSideProps: GetServerSideProps = async (props) => {
-	
+	const prisma = new PrismaClient()
 	const PAGE_SIZE = 25;
 	const PAGE = 1;
 	
-	const  { result : data} = await filter(PAGE , PAGE_SIZE , PAGE , PAGE_SIZE)
-	console.log('this', data)
+	const  { result : data} = await filter(PAGE , PAGE_SIZE , PAGE , PAGE_SIZE , prisma)
+	console.log(data)
 	const {projectBills , anotherBills} = data
 	return {
 		props: {
